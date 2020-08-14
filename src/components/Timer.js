077148e0fd5a9c9e812d3d342adc0b4a83bb6bc7
Arom Jhee <aromjhee@gmail.com>
2020-08-14 13:10:44 -0700

    stop and load working

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect, useCallback } from 'react';
 import PieChart from './PieChart';
 import Session from './Session';
 import url from './url';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 export default function Timer() {
   const [sessions, setSessions] = useState([])
+  const [restart, setRestart] = useState(false)
   const [hours, setHours] = useState(0)
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
+  const [showAlarm, setShowAlarm] = useState(false)
 
   function timeDisplay(s) {
     return s === 0 ? '00' : s < 10 ? `${0}${s}` : s;
@@ -33,18 +37,19 @@ export default function Timer() {
     (async function fetchSessions() {
       const res = await fetch(url);
       const json = await res.json();
+      console.log('=============', json.sessions)
       setSessions(json.sessions)
       loadTimer(json.sessions)
     })();
-  }, [loadTimer])
+  }, [loadTimer, restart])
 
 
   useEffect(() => {
-    if (isRunning) {
-      const id = setInterval(() => {
+    if (isRunning && !showAlarm) {
+      const intervalId = setInterval(() => {
         setSeconds(seconds => seconds - 1)
       }, 100)
-
+      console.log('^^^^^^^^^^^^^^^', seconds)
       if (hours > 0 && minutes === 0 && seconds === 0) {
         setHours(hours - 1)
         setMinutes(59)
@@ -53,29 +58,64 @@ export default function Timer() {
         setMinutes(minutes - 1)
         setSeconds(59)
       } else if (hours === 0 && minutes === 0 && seconds === 0) {
-        if (sessions.length > 0) {
-          sessions.shift()
-          if (sessions.length === 0) {
-            setIsRunning(false)
-            clearInterval(id)
-          } else {
-            loadTimer(sessions)
-          }
+        setShowAlarm(true)
+
+        const newSessions = sessions.filter((session, i) => i !== 0)
+        setSessions(newSessions)
+        if (newSessions.length > 0) {
+          loadTimer(newSessions)
+        } else {
+          clearInterval(intervalId)
+          setIsRunning(false)
+          setRestart(!restart)
         }
+        
+        // const a = sessions.shift()
+        // const newSessions = sessions.filter((session, i) => i !== 0)
+        // const newSessions = sessions.filter((session, i) => session !== a)
+        // setSessions(newSessions)
+        // console.log('--------------', sessions)
+        // if (newSessions.length === 0) {
+        //   console.log('+++++++++++++++++')
+        //   clearInterval(intervalId)
+        //   setRestart(true)
+        //   setIsRunning(false)
+        // } else {
+        //   console.log('*************', sessions)
+        //   console.log('*************', newSessions)
+        //   setSessions(newSessions)
+        //   setIsRunning(false)
+        //   setShowAlarm(true)
+        //   loadTimer(newSessions)
+        // }
       }
 
-      return () => clearInterval(id)
+      return () => clearInterval(intervalId)
     }
-  }, [isRunning, hours, minutes, seconds, sessions, loadTimer])
-
-  // useEffect(() => {
-  //   if (sessions.length > 0 && sessions[0].duration !== `${hours}:${minutes}:${seconds}`) {
-  //     loadTimer(sessions)
-  //   }
-  // },[sessions, hours, minutes, seconds])
+  }, [isRunning, hours, minutes, seconds, sessions, loadTimer, showAlarm])
 
   return (
     <div className='h-screen grid grid-rows-8 grid-cols-3'>
+      {showAlarm ? 
+        (<div className='modal bg-black z-10 absolute inset-0 flex justify-center items-center'>
+          <div className='bg-gray-900 radius-custom'>
+            <div className='m-16 flex flex-col justify-center'>
+              <h4 className='text-6xl mb-12 p-16'>
+                <div className='alarm-bell'>
+                  <FontAwesomeIcon icon={faBell} size='5x' color='orange' />
+                </div>
+              </h4>
+              <button
+                className='text-6xl mx-16 px-5 py-10 border-orange-900 border-8 radius-custom'
+                onClick={() => {
+                  setShowAlarm(false)
+                  // setIsRunning(true)
+                }}>STOP</button>
+            </div>
+          </div>
+        </div>) 
+        : null
+      }
       <PieChart />
       <p className='text-2xl row-end-3 row-span-3 col-start-1 col-span-3 flex justify-center items-end'>***Time is set to decrease by 100ms***</p>
       <div className='row-start-3 row-span-1 col-start-1 col-span-3 text-4xl flex flex-col justify-evenly align-center items-center'>
