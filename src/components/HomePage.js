cab71e70e4a68d25807317fa39142d5699654b4c
Arom Jhee <aromjhee@gmail.com>
2020-08-21 15:17:06 -0700

    start homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import image from '../image/home.jpg';
+// import image from '../image/home.jpg';
 
 export default function HomePage() {
+  const [showGetStarted, setShowGetStarted] = useState(false)
+
+  useEffect(() => {
+    
+  })
+
   return (
-    <div>
-      <button>
+    <div className='custom-background-image h-screen'>
+      <button className='text-6xl text-red-100'>
         <Link to='/loopout'>Get Started</Link>
       </button>
     </div>
