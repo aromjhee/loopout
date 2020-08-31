cc78222ed46552bc19a624104ff1e19bb4e5a37e
Arom Jhee <aromjhee@gmail.com>
2020-08-31 16:35:36 -0700

    wip homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -16,7 +16,7 @@ export default function HomePage() {
       <div className='flex justify-center items-center text-4xl row-span-1 font-sans font-light letter-spacing'>Give Away Control.</div>
       <div className='flex justify-center items-baseline text-5xl row-span-1 font-sans font-thin italic letter-spacing'>
         Create Your 
-        <span className='text-5xl glitch-1'>Loop</span>
+        <span className='text-5xl glitch-1'>Loop.</span>
       </div>
       <div className='flex justify-center items-center row-span-1'>
         <button className='flex justify-center items-center home-button-style font-mono font-extrabold animate-pulse start-text'>
