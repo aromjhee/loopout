fb2b8dae9dcb55e715faaa74003042f4715e4d8b
Arom Jhee <aromjhee@gmail.com>
2020-09-03 14:33:01 -0700

    wip log in and register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function LogIn() {
+export default function Register() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [rePassword, setRePassword] = useState('')
 
-  const login = () => {
-
-  }
-
-  const demoLogin = () => {
+  const register = () => {
 
   }
 
@@ -36,20 +33,18 @@ export default function LogIn() {
             <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id='inline-password' type='password' placeholder='*********' />
           </div>
         </div>
-        <div className='md:flex md:items-center mb-32 justify-center'>
-          <div className='md:w-1/3'></div>
-          <div className='md:w-2/3 flex justify-between'>
-            <button className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full' type='button' onClick={() => login()}>
-              Login
-            </button>
-            <button className='shadow bg-teal-500 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full' type='button'
-            onClick={() => demoLogin()}>
-              Demo Log-in
-            </button>
+        <div className='md:flex md:items-center mb-32'>
+          <div className='md:w-1/3'>
+            <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='inline-password'>
+              Re-Enter Password
+            </label>
+          </div>
+          <div className='md:w-2/3'>
+            <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id='inline-password' type='password' placeholder='*********' />
           </div>
         </div>
         <div className='md:flex md:items-center flex-col'>
-          <div className='md:w-2/3 text-red-300 mb-10 flex justify-center'>Don't have an account?</div>
+          <div className='md:w-2/3 text-red-300 mb-10 flex justify-center'></div>
           <div className='md:w-2/3 flex justify-center'>
             <Link to='/register'>
               <button className='shadow bg-orange-500 hover:bg-orange-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full' type='button'>
