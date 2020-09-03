3a624b594bbf382aef7115263d63d05669662b77
Arom Jhee <aromjhee@gmail.com>
2020-09-03 16:52:44 -0700

    wip

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -50,50 +50,51 @@ export default function LogIn() {
           ))
         ) : null
       }
-      <form className='w-5/6 text-4xl mr-16'>
-        <div className='md:flex md:items-center mb-16'>
-          <div className='md:w-1/3'>
-            <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='email'>
-              E-mail
-            </label>
+      <div className='w-5/6 flex justify-center'>
+        <form className='w-5/6 text-4xl'>
+          <div className='md:flex md:items-center mb-16'>
+            <div className='md:w-1/3'>
+              <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='email'>
+                E-mail
+              </label>
+            </div>
+            <div className='md:w-2/3'>
+              <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id='email' type='email' placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
           </div>
-          <div className='md:w-2/3'>
-            <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id='email' type='email' placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <div className='md:flex md:items-center mb-24'>
+            <div className='md:w-1/3'>
+              <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='password'>
+                Password
+              </label>
+            </div>
+            <div className='md:w-2/3'>
+              <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id='password' type='password' placeholder='*********' autoComplete='on' value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
           </div>
-        </div>
-        <div className='md:flex md:items-center mb-24'>
-          <div className='md:w-1/3'>
-            <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='password'>
-              Password
-            </label>
-          </div>
-          <div className='md:w-2/3'>
-            <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id='password' type='password' placeholder='*********' autoComplete='on' value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-        </div>
-        <div className='md:flex md:items-center mb-32 justify-center'>
-          <div className='md:w-1/3'></div>
-          <div className='md:w-2/3 flex justify-between'>
-            <button className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full' type='button' onClick={() => login()}>
-              Login
-            </button>
-            <button className='shadow bg-teal-500 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full' type='button'
-            onClick={() => demoLogin()}>
-              Demo Log-in
-            </button>
-          </div>
-        </div>
-        <div className='md:flex md:items-center flex-col'>
-          <div className='md:w-2/3 text-red-300 mb-10 flex justify-center'>Don't have an account?</div>
-          <div className='md:w-2/3 flex justify-center'>
-            <Link to='/register'>
-              <button className='shadow bg-orange-500 hover:bg-orange-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full' type='button'>
-                Register
+          <div className='md:flex md:items-center mb-32 justify-center'>
+            <div className='md:w-2/3 flex flex-col justify-center'>
+              <button className='shadow bg-purple-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full mb-16' type='button' onClick={() => login()}>
+                Log-in
               </button>
-            </Link>
+              <button className='shadow bg-teal-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full' type='button'
+              onClick={() => demoLogin()}>
+                Demo Log-in
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+          <div className='md:flex md:items-center flex-col'>
+            <div className='md:w-2/3 text-red-300 mb-10 flex justify-center'>Don't have an account?</div>
+            <div className='md:w-2/3 flex justify-center'>
+              <Link to='/register'>
+                <button className='shadow bg-orange-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded-full' type='button'>
+                  Register
+                </button>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
