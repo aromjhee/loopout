0029a143c562c0b4bb693a5aa97341aed9f511e7
Arom Jhee <aromjhee@gmail.com>
2020-09-03 17:07:56 -0700

    wip Register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -71,11 +71,11 @@ export default function Register() {
           <div className='md:flex md:items-center mb-24'>
             <div className='md:w-1/3 text-4xl'>
               <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4' htmlFor='repassword'>
-                Re-Enter Password
+                Confirm Password
               </label>
             </div>
             <div className='md:w-2/3'>
-              <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id='repassword' type='password' placeholder='*********' autoComplete='on' value={password} onChange={(e) => setRePassword(e.target.value)} />
+              <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id='repassword' type='password' placeholder='*********' autoComplete='on' value={rePassword} onChange={(e) => setRePassword(e.target.value)} />
             </div>
           </div>
           <div className='md:flex md:items-center mb-32 justify-center'>
