a7989ffeac19743737e184a69ef32875cd0b1d9f
Arom Jhee <aromjhee@gmail.com>
2020-09-20 16:57:49 -0700

    configure url so don't have to keep switching

--- src/components/url.js
+++ src/components/url.js
@@ -1,4 +1,9 @@
-// const url = 'http://localhost:5000/'
-const url = 'https://loopout-backend.herokuapp.com/'
+let url;
+
+if (process.env.NODE_ENV === 'production') {
+  url = 'https://loopout-backend.herokuapp.com/'
+} else {
+  url = 'http://localhost:5000/'
+}
 
 export default url
